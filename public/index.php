655c8af63ea81ed4da19a12ec70ae79045081e86
Daniela Kleemann <dani@elaweb.dev>
2026-05-27 06:22:37 +0200

    Register the autoloader

--- public/index.php
+++ public/index.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
+use SweetBlog\Autoloader;
+
+require __DIR__ . '/../src/Autoloader.php';
+
+$autoloader = new Autoloader();
+$autoloader->register();
+
 echo 'Hello, world!';
