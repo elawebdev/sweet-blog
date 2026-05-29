4403447585b318665e69f97db5baeee932364582
Daniela Kleemann <dani@elaweb.dev>
2026-05-29 17:26:55 +0200

    Bind View class

--- src/Application.php
+++ src/Application.php
@@ -9,6 +9,7 @@ use SweetBlog\Core\Http\StatusCode;
 use SweetBlog\Core\Router\Dispatcher;
 use SweetBlog\Core\Router\Exceptions\RouteNotFoundException;
 use SweetBlog\Core\Router\Resolver;
+use SweetBlog\Core\View\View;
 
 /**
  * Initializes and runs the application.
@@ -22,6 +23,7 @@ final readonly class Application
     public function run(): void
     {
         $container = new Container();
+        $container->bind(View::class, fn() => new View("$this->rootDirectory/templates"));
 
         try {
             $handler = new Resolver()->match();
