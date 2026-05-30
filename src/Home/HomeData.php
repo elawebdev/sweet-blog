4d231398ac506bbd1e268a234e263015f589e97c
Daniela Kleemann <dani@elaweb.dev>
2026-05-30 11:06:23 +0200

    Fetch recent posts

--- src/Home/HomeData.php
+++ src/Home/HomeData.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace SweetBlog\Home;
 
 use SweetBlog\Core\View\ViewData;
+use SweetBlog\Entities\PostListItemEntity;
 
 final readonly class HomeData implements ViewData
 {
+    /**
+     * @param string $title Post title
+     * @param array<PostListItemEntity> $postList List of recent posts
+     */
     public function __construct(
         public string $title,
+        public array $postList,
     ) {}
 }
--- src/Home/HomeHandler.php
+++ src/Home/HomeHandler.php
@@ -9,6 +9,7 @@ use SweetBlog\Core\Http\Body;
 use SweetBlog\Core\Http\Response;
 use SweetBlog\Core\Http\StatusCode;
 use SweetBlog\Core\View\View;
+use SweetBlog\Repositories\PostListRepository;
 
 /**
  * Home page handler.
@@ -17,12 +18,16 @@ final readonly class HomeHandler implements Handler
 {
     public function __construct(
         private View $view,
+        private PostListRepository $postListRepository,
     ) {}
 
     public function execute(): Response
     {
+        $postList = $this->postListRepository->fetchRecentPosts(limit: 10);
+
         $data = new HomeData(
             title: 'Home Page',
+            postList: $postList,
         );
 
         $content = $this->view->render('home', $data);
